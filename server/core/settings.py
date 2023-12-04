513e18bfbb3681b6edf9cf9814778d96e7b678c9
taa <TahaM8000@gmail.com>
2023-12-04 22:46:45 +0330

    update server for deploy

--- server/core/settings.py
+++ server/core/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'data/db.sqlite3',
     }
 }
 
