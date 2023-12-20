4d8bdf970727e81b40d437c3132b06663daedcaa
Mors <seyedsahel1383@gmail.com>
2023-12-20 19:38:04 +0330

    Finding text keywords with the help of nltk library

--- server/websites/views.py
+++ server/websites/views.py
@@ -17,9 +17,22 @@ def FindWebsiteCategory(website):
     website.save()
 # ----------------------------------web scraping---------------------------------------------------------------------------------------------
 def scrap(given_url,given_categories):
+    def find_keywords(text):
+        tokens = word_tokenize(text)
+    # حذف کلمات توقفی
+        stop_words = set(stopwords.words('english'))  # تنظیم زبان متن مورد نظر خود
+        filtered_tokens = [word for word in tokens if word.lower() not in stop_words]
+        fdist = FreqDist(filtered_tokens)
+        keywords = fdist.most_common(12)  # تعداد کلمات کلیدی 
+        return keywords
+
+
     url=given_url
     
     title=''
+    cleans=[]
+    count=0
+    junk=['از','به','با','برای','در','هم','و','اگر','همراه','حتما','قطعا','کاملا','است','هست','اره','هستید','دنبال','،','را','لطفا','منتظر','بمانید']
     page=requests.get(url)
 
     if page.status_code == 200:
@@ -33,8 +46,17 @@ def scrap(given_url,given_categories):
         class_name = paragraph.get('class') #گرفتن اسم کلاس های تگ p
         if class_name != 'None':
             p_text=soup.find('p',class_=class_name).text#پیدا کردن متن توی تگ
-        
+            keywords = find_keywords(p_text)
+            for keyword in keywords: 
+                temp = keyword[0] 
+                if temp in junk: 
+                    count+=1 
+            
+                else: 
+                    cleans.append(keyword)
     else:
         print(page.status_code)
 
-    return p_text
+       
+
+    return cleans
