bd4ecf3a0e76a6d0b6a26b5e29959f70c6b1ffea
Mors <seyedsahel1383@gmail.com>
2024-01-20 23:40:10 +0330

    Make the code more practical

--- utils/driver.py
+++ utils/driver.py
@@ -2,42 +2,41 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
+def find_text(url):
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
 
-chrome_options = Options()
-chrome_options.add_argument("--headless")
+    options = webdriver.ChromeOptions()
 
-options = webdriver.ChromeOptions()
-
-driver = webdriver.Chrome(options=chrome_options)
-driver.get('https://faradars.org/')
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get(url)
 
 
-wait = WebDriverWait(driver, 20)
-print(driver.title)
-page_content = driver.page_source
-soup = BeautifulSoup(page_content, 'html.parser')
-p_text=driver.title+""
+    wait = WebDriverWait(driver, 20)
+    print(driver.title)
+    page_content = driver.page_source
+    soup = BeautifulSoup(page_content, 'html.parser')
+    text=driver.title+""
 
-h1 = soup.find("h1")
-if h1 :
-    h1=h1.text
-    p_text=p_text+h1+","
+    h1 = soup.find("h1")
+    if h1 :
+        h1=h1.text
+        text=text+h1+","
+      
+    h2 = soup.find("h2")
+    if h2 :
+        h2=h2.text
+        text=text+h2+","
     
-    print(p_text)
-h2 = soup.find("h2")
-if h2 :
-    h2=h2.text
-    p_text=p_text+h2+","
-   
-    print(p_text)
-h3 = soup.find("h3")
-if h3 :
-    h3=h3.text
-    p_text=p_text+h3+","
-    print(p_text)
-strong = soup.find("strong")
-if strong :
-    strong=strong.text
-    p_text=p_text+strong
-print(p_text)
-driver.quit()
+    h3 = soup.find("h3")
+    if h3 :
+        h3=h3.text
+        text=text+h3+","
+        
+    strong = soup.find("strong")
+    if strong :
+        strong=strong.text
+        text=text+strong
+    print(text)
+    driver.quit()
+    return text
