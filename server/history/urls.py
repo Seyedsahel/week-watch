c0952bb36a8ead98db76219761749b55a8dd5ca5
taa <TahaM8000@gmail.com>
2024-02-03 07:05:23 +0330

    category hours

--- server/history/urls.py
+++ server/history/urls.py
@@ -4,4 +4,6 @@ from .views import *
 urlpatterns = [
     path('record/',GetRecord),
     path('record/onWebsite/',get_hours_on_website),
+    path('record/onCategoryWebsites/',get_hours_on_category_websites),
+    path('record/onCategory/',get_hours_on_category),
 ]
--- server/history/views.py
+++ server/history/views.py
@@ -1,4 +1,3 @@
-from email.message import EmailMessage
 from .serializers import *
 from .models import *
 from django.http import HttpResponse, JsonResponse
@@ -6,7 +5,7 @@ from .models import *
 from websites.models import Website,WebSiteCategory
 from websites.views import FindWebsiteCategory
 import threading
-from django.db.models import Sum
+from django.db.models import ExpressionWrapper, F, DurationField, Sum
 from datetime import datetime
 # -------------------------------------------------------------------------------------------------------------------------------
 def GetRecord(request):
@@ -92,17 +91,78 @@ def GetRecord(request):
                 record = HistoryRecord.objects.filter(link=link,user=user)
             except HistoryRecord.DoesNotExist:
                 return JsonResponse({'message': 'record not exist.'}, status=400)
-            
-            
 
-        
 
 
         return JsonResponse({'message': 'link recorded.'})
     else:
         return JsonResponse({'message': 'Only POST method is allowed.'}, status=405)
-# -------------------------------------------------------------------------------------------------------------------------------
-from django.db.models import ExpressionWrapper, F, DurationField, Sum
+#-------------------------------------------------------------------------------------------------------------------------------
+def get_hours_on_category_websites(request):
+    if request.method == "POST":
+        data = request.POST
+        print(data)
+
+
+        category_name = data.get("category_name")
+        email = data.get("email").lower()
+
+        if not email:
+            return JsonResponse({'message': 'email is empty.'}, status=400)
+        
+        if not category_name:
+            return JsonResponse({'message': 'category_name is empty.'}, status=400)
+
+
+        if(email != request.user.email and not request.user.is_admin):
+            return JsonResponse({'message': 'not allowed!'}, status=400)
+
+    
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            return JsonResponse({'message': 'user not found!'}, status=404)
+        
+        
+        try:
+            category = WebSiteCategory.objects.get(name=category_name)
+        except Website.DoesNotExist:
+            return JsonResponse({'message': 'category not found!'}, status=404)
+
+        websites = category.websites.all()
+
+        print(websites)
+        duration = 0
+
+        data = []
+
+        for website in websites:
+            duration = HistoryRecord.objects.filter(user=user, website=website).aggregate(
+                total_duration=Sum(
+                    ExpressionWrapper(F('closed') - F('created'), output_field=DurationField())
+                )
+            )
+            
+            total_duration = duration['total_duration']
+            
+            if total_duration is not None:
+                hours = total_duration.seconds // 3600
+                minutes = (total_duration.seconds % 3600) // 60
+                seconds = total_duration.seconds % 60
+                
+                website_data = {
+                    'website': website.domain,
+                    'hours': hours,
+                    'minutes': minutes,
+                    'seconds': seconds,
+                }
+                
+                data.append(website_data)
+
+        return JsonResponse({'message': data})
+    else:
+        return JsonResponse({'message': 'Only POST method is allowed.'}, status=405)
+#-------------------------------------------------------------------------------------------------------------------------------
 def get_hours_on_website(request):
     if request.method == "POST":
         data = request.POST
@@ -157,3 +217,67 @@ def get_hours_on_website(request):
         return JsonResponse({'message': data})
     else:
         return JsonResponse({'message': 'Only POST method is allowed.'}, status=405)
+#-------------------------------------------------------------------------------------------------------------------------------
+def get_hours_on_category(request):
+    if request.method == "POST":
+        data = request.POST
+        print(data)
+
+
+        category_name = data.get("category_name")
+        email = data.get("email").lower()
+
+        if not email:
+            return JsonResponse({'message': 'email is empty.'}, status=400)
+        
+        if not category_name:
+            return JsonResponse({'message': 'category_name is empty.'}, status=400)
+
+
+        if(email != request.user.email and not request.user.is_admin):
+            return JsonResponse({'message': 'not allowed!'}, status=400)
+
+    
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            return JsonResponse({'message': 'user not found!'}, status=404)
+        
+        
+        try:
+            category = WebSiteCategory.objects.get(name=category_name)
+        except Website.DoesNotExist:
+            return JsonResponse({'message': 'category not found!'}, status=404)
+
+        websites = category.websites.all()
+
+        cat_data = {
+                    'name': category_name,
+                    'hours': 0,
+                    'minutes': 0,
+                    'seconds': 0,
+        }
+
+        for website in websites:
+            duration = HistoryRecord.objects.filter(user=user, website=website).aggregate(
+                total_duration=Sum(
+                    ExpressionWrapper(F('closed') - F('created'), output_field=DurationField())
+                )
+            )
+            
+            total_duration = duration['total_duration']
+            
+            if total_duration is not None:
+                hours = total_duration.seconds // 3600
+                minutes = (total_duration.seconds % 3600) // 60
+                seconds = total_duration.seconds % 60
+                
+                
+                cat_data['hours'] += hours
+                cat_data['minutes'] += minutes
+                cat_data['seconds'] += seconds
+
+        return JsonResponse({'message': cat_data})
+    else:
+        return JsonResponse({'message': 'Only POST method is allowed.'}, status=405)
+#-------------------------------------------------------------------------------------------------------------------------------
